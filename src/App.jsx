01d511fd57512f4ea701616f89221daad17df4df
Raul Soneira <raul.soneira@cirvianum.cat>
2024-05-14 18:09:09 +0200

    acabat

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   const prova = [3,6];
   return (
     <div className="App">
-      <LuckyN numDaus={2} suma={7}/>
+      <LuckyN numDaus={2}/>
+       
+
     </div>
   );
 }
--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -2,11 +2,45 @@ import React, { useState } from 'react';
 import Dice from './Dice';
 import { getTirades, sum } from './utils';
 
-
-const LuckyN = ({ numDaus, suma }) => {
+const LuckyN = ({ numDaus }) => {
   const [dice, setDice] = useState(getTirades(numDaus));
-  const isWinner = sum(dice) === suma;
-  
+
+  const [guess, setGuess] = useState(1);
+
+  const isWinner = sum(dice) === guess;
+
+  const handleClick = () => {
+    setDice(getTirades(numDaus));
+  };
+
+  const handleGuessChange = (event) => {
+    setGuess(parseInt(event.target.value));
+  };
+
+  return (
+    <main>
+      <h1 className="text-x1 mt-3 text-center text-blue-500">
+        Prova sort! La suma ha de donar {guess}
+      </h1>
+      <label for="guessInput">Número a encertar:</label>
+      <input
+        id="guessInput"
+        type="number"
+        onChange={handleGuessChange}
+        value={guess}
+        className="mt-3 text-center text-pink-500" />
+      <Dice dice={dice} />
+      {isWinner ? (
+        <p className="text-center text-2xl text-green-500">Has guanyat!</p>
+      ) : null}
+      <button
+        className="mx-auto mt-3 block rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-800"
+        onClick={handleClick}
+      >
+        Tira els daus
+      </button>
+    </main>
+  );
 };
 
 export default LuckyN;
